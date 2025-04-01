7107e56b2c0a1cb7bff1cda1b665f1917440269d
Mylène Bergamini <mylene.devin@laposte.net>
2025-04-01 14:42:00 +0200

    mise en place css (à terminer)

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,51 @@
-import { useState } from 'react'
+
 import './App.css'
 
 const recipes = [
-  { name: " Cookies au beurre de cacahuète ", difficulty: "facile" },
-  { name: " Macaron framboisier ", difficulty: "difficile" },
-  { name: " Tarte au citron merignuée ", difficulty: "moyen" },
-  { name: " Amandier ", difficulty: "facile" }
+  {id:1, name:" Cookies au beurre de cacahuète ", difficulty: "facile"},
+  {id:2, name: " Macaron framboisier ", difficulty: "difficile" },
+  {id:3, name: " Tarte au citron merignuée ", difficulty: "moyen" },
+  {id:4, name: " Amandier ", difficulty: "facile" }
 ];
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <div className='menu'>
-      <h2>Accueil</h2>
-      <ul>{recipes.map((recipe, index) => (
-        <li key={index}>{recipe.name}</li>
-      ))}
-      </ul>
-    </div>
-
-    <div className='mainContent'>
-      <div className='connexionBar'>
-        <input type="email" placeholder='Adresse Email'/>
-        <input type="password" placeholder='mot de passe' />
-        <button>OK</button>
+    <div className='Container'>
+      <div className='menu'>
+        <h2>Accueil</h2>
+        <ul>{recipes.map((recipe) => (
+          <li key={recipe.id}>{recipe.name}</li>
+        ))}
+        </ul>
       </div>
-    </div>
 
-    <h1>Les recettes Orecipes</h1>
-    <p>Voici nos {recipes.length} recettes</p>
-
-    <div className="gridRecipes">
-      {recipes.map((recipes, index)=> (
-        <div key={index} className='cardRecipe'>
-          <h3>{recipes.name}</h3>
-          <p>Difficulté : {recipes.difficulty}</p>
-          <a href="#"className='btn'>Voir la recette</a>
+      <div className='mainContent'>
+        <div className='connexionBar'>
+          <input type="email" placeholder='Adresse Email' />
+          <input type="password" placeholder='mot de passe' />
+          <button>OK</button>
         </div>
-      ))}
-    </div>
-</>
-    
+     
+
+      <h1>Les recettes oRecipes</h1>
+      <p>Voici nos {recipes.length} recettes</p>
+
+      <div className="gridRecipes">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className='cardRecipe'>
+            <h3>{recipe.name}</h3>
+            <p>Difficulté : {recipe.difficulty}</p>
+            <a href="#" className='btn'>Voir la recette</a>
+          </div>
+        ))}
+      </div> 
+      </div>
+      </div>
+    </>
+
 
   )
 }
